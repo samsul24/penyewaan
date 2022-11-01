64b4b58bba6e7e0fc9ed759c09e2363bd53616cc
Samsul <samsulib30@gmail.com>
2022-11-01 21:53:45 +0700

    final

--- database/penyewaan.sql
+++ database/penyewaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2022 at 10:57 AM
+-- Generation Time: Nov 01, 2022 at 03:53 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -20,6 +20,67 @@ SET time_zone = "+00:00";
 --
 -- Database: `penyewaan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_sewa`
+--
+
+CREATE TABLE `data_sewa` (
+  `id_sewa` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `id_lapangan` int(11) NOT NULL,
+  `tanggal` varchar(20) NOT NULL,
+  `jam_mulai` time DEFAULT NULL,
+  `durasi` int(11) NOT NULL,
+  `jam_selesai` time DEFAULT NULL,
+  `status` enum('disetujui','belum_disetujui') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_sewa`
+--
+
+INSERT INTO `data_sewa` (`id_sewa`, `id_user`, `nama`, `id_lapangan`, `tanggal`, `jam_mulai`, `durasi`, `jam_selesai`, `status`) VALUES
+(19, 2, 'user', 1, '2022-10-30', '07:00:00', 2, '09:00:00', 'disetujui'),
+(21, 2, 'user', 2, '2022-10-30', '09:00:00', 3, '12:00:00', 'disetujui');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jam`
+--
+
+CREATE TABLE `jam` (
+  `id` int(11) NOT NULL,
+  `jam` varchar(50) NOT NULL,
+  `is_available` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jam`
+--
+
+INSERT INTO `jam` (`id`, `jam`, `is_available`) VALUES
+(2, '07:00:00', 1),
+(3, '08:00:00', 1),
+(4, '09:00:00', 1),
+(5, '10:00:00', 1),
+(6, '11:00:00', 1),
+(7, '12:00:00', 1),
+(8, '13:00:00', 1),
+(9, '14:00:00', 1),
+(10, '15:00:00', 1),
+(11, '16:00:00', 1),
+(12, '17:00:00', 1),
+(13, '18:00:00', 1),
+(14, '19:00:00', 1),
+(15, '20:00:00', 1),
+(16, '21:00:00', 1),
+(17, '22:00:00', 1),
+(18, '23:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -41,7 +102,8 @@ CREATE TABLE `lapangan` (
 --
 
 INSERT INTO `lapangan` (`id_lapangan`, `nama_lapangan`, `gambar_lapangan`, `deskripsi_lapangan`, `status_lapangan`, `date`) VALUES
-(1, 'tombro 1', '', 'luas 100 hektar ', 'tersedia', '0000-00-00 00:00:00');
+(1, 'tombro jos', 'lapangan1.jpg', 'luas 100 hektar oke', 'tersedia', '0000-00-00 00:00:00'),
+(2, 'tombro 2', '6353ab9e023bd.jpg', 'luas 200 x 400 meter', 'tersedia', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -61,7 +123,21 @@ CREATE TABLE `pengumuman` (
 --
 
 INSERT INTO `pengumuman` (`id`, `objek`, `deskripsi`, `status`) VALUES
-(1, 'Kehilangan', 'Tas berwarna hitam dengan motih doraemon dan didalamnya berisikan sejumlah berkas dan hp dengan merek xiomi', 'belum_selesai');
+(1, 'Kehilangan Tas', 'Tas berwarna hitam dengan motih doraemon dan didalamnya berisikan sejumlah berkas dan hp dengan merek xiomi', 'selesai'),
+(3, 'Kehilangan Hp', 'Merek Xiomi warna hitam gold layar agak sedikit retak', 'belum_selesai'),
+(4, 'Kehilangan Buku', 'Buku mapel B indonesia kelas 3 Sma berjumlah 2 buku ', 'belum_selesai');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trash`
+--
+
+CREATE TABLE `trash` (
+  `id` int(11) NOT NULL,
+  `id_sewa` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -74,7 +150,7 @@ CREATE TABLE `user` (
   `nama_lengkap` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `no_telp` int(50) NOT NULL,
+  `no_telp` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `level` enum('admin','customer') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -84,11 +160,28 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama_lengkap`, `username`, `password`, `no_telp`, `email`, `level`) VALUES
-(1, 'admin', 'admin', 'admin', 2147483647, 'admin@gmail.com', 'admin');
+(1, 'admin', 'admin', 'admin', '089987657543', 'admin@gmail.com', 'admin'),
+(2, 'user eko', 'user', 'user', '085656787', 'user@gmail.com', 'customer'),
+(3, 'qwe', 'qwe', 'user', 'qwe', 'qwe', 'customer'),
+(4, 'asd', 'asd', 'asd', '123', 'asd', 'customer');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `data_sewa`
+--
+ALTER TABLE `data_sewa`
+  ADD PRIMARY KEY (`id_sewa`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_lapangan` (`id_lapangan`);
+
+--
+-- Indexes for table `jam`
+--
+ALTER TABLE `jam`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `lapangan`
@@ -103,6 +196,12 @@ ALTER TABLE `pengumuman`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `trash`
+--
+ALTER TABLE `trash`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -113,22 +212,51 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `data_sewa`
+--
+ALTER TABLE `data_sewa`
+  MODIFY `id_sewa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `jam`
+--
+ALTER TABLE `jam`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
 -- AUTO_INCREMENT for table `lapangan`
 --
 ALTER TABLE `lapangan`
-  MODIFY `id_lapangan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_lapangan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pengumuman`
 --
 ALTER TABLE `pengumuman`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `trash`
+--
+ALTER TABLE `trash`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `data_sewa`
+--
+ALTER TABLE `data_sewa`
+  ADD CONSTRAINT `data_sewa_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `data_sewa_ibfk_2` FOREIGN KEY (`id_lapangan`) REFERENCES `lapangan` (`id_lapangan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
